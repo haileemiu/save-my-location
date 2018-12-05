cd86c47e3c33431aba8b9ff3c3fc0ebb8c80eaaa
Hailee Miu <hailee.miu@gmail.com>
2018-12-04 19:56:58 -0600

    console log geolocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // Click event handler for getting geolocation
+  getLocation = () => {
+    if (navigator.geolocation) {
+      // timeout at 60000 milliseconds (60 seconds)
+      const options = { timeout: 60000 };
+      navigator.geolocation.getCurrentPosition(
+        this.showLocation,
+        this.errorHandler,
+        options,
+      );
+    } else {
+      console.log('Sorry, browser does not support geolocation!');
+    }
+  }
+
+  // Success callback for getCurrentPosition
+  showLocation = (position) => {
+    const { latitude, longitude } = position.coords;
+
+    console.log(`Latitude : ${latitude} Longitude: ${longitude}`);
+  }
+
+  // Error callback for getCurrentPosition
+  errorHandler = (err) => {
+    if (err.code === 1) {
+      console.log('Error: Access is denied!');
+    } else if (err.code === 2) {
+      console.log('Error: Position is unavailable!');
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Save My Location</h1>
+
+        <button onClick={this.getLocation} type="button">Where am I?</button>
+
       </div>
     );
   }
